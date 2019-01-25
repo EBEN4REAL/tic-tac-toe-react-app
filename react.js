8647c11f9e13084139d8aa5e1b5c28c6087d24f9
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-01-25 18:59:10 +0100

    Prepared components for the time travel feature

--- react.js
+++ react.js
@@ -56,9 +56,10 @@ class Game extends React.Component {
          squares: Array(9).fill(null),
        }],
     xIsNext: true,
+    stepNumber: 0,
   }
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares)) {
@@ -70,15 +71,20 @@ class Game extends React.Component {
         squares: squares,
       }]),
       xIsNext: !this.state.xIsNext,
+      stepNumber: history.length,
     });
   }
-  jumpTo = (move) => {
-     
+  jumpTo = (step) => {
+     this.setState({
+       stepNumber: step,
+       xIsNext: (step % 2) === 0,
+     });
   }
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
-
+    console.log(history);
+    const current = history[this.state.stepNumber];
+    console.log(current);
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
