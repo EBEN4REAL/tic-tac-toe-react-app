fa2ec1d0c18b13b80d0599102e1783369a6308b8
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-01-25 15:56:43 +0100

    Added player change aaoto filling of boxes

--- react.js
+++ react.js
@@ -29,6 +29,9 @@ class Board extends React.Component {
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
+     if (calculateWinner(squares)) {
+       return;
+     }
     squares[i] = this.state.xIsNext ? 'X' : 'O'; 
     this.setState({
       squares: squares,
@@ -43,7 +46,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+     const winner = calculateWinner(this.state.squares);
+     let status;
+     if (winner) {
+       status = 'Winner: ' + winner;
+     } else {
+       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+     }
 
     return (
       <div>
@@ -87,6 +96,29 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for(let i = 0; i < lines.length; i++){
+   const  [a,b,c] = lines[i];
+   console.log(a,b,c);
+   console.log(squares[a]);
+   if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+     return squares[a];
+   }
+  }
+  return null;
 }
 
 // ========================================
